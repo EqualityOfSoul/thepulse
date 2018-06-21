92f1357afd7b002547ad2e37e12e413e51dd18d0
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-06-21 22:28:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on('message', async (message) => {
 
     
     if (message.author.bot) return;
-  //  if (message.author.id === '321268938728144906') return message.chanel.send("доступ ограничен");
+    if (message.author.id === '369471128835457026') return message.reply("доступ ограничен");
     //Отвечает за установку префикса в команды
     let prefixes = ['X1', 'X!', 'X@', 'x1', 'x!', 'x@','<@441667160025333762>'];
     let prefix = false;
