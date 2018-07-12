32416fb3d43db03ae3f16d7492118e8fea23bc85
Dzhemesyuk Artem <zziger07@gmail.com>
2018-07-12 17:58:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -182,8 +182,7 @@ client.on('message', async (message) => {
 	return target.replace(new RegExp(search, 'g'), replacement);
 	};
 	
-	if (message.channel.id === '461764710308642817' && message.author.id === '337573905722114051') return message.channel.send('тут тебе пользоваться мной низя >:(');
-    //Эмулирует произвольный код из аккаунта.
+	    //Эмулирует произвольный код из аккаунта.
     if (['eval', 'эмулировать'].includes(command) && (message.author.id === "361951318929309707" || message.author.id === "421030089732653057" || message.author.id === "242091351951409152")) {
 	    actOWN = actOWN + 1;actALL = actALL +1;
 
