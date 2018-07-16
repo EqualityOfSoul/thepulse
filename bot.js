92ea761719212fa84be9dcbe6e76eb866df0ec5c
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-07-16 22:32:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,8 @@ let actNSFW = 0; // actNSFW = actNSFW + 1;actALL = actALL +1;
 let actOWN = 0; //  actOWN = actOWN + 1;actALL = actALL +1;
 let actIMG = 0; //  actIMG = actIMG + 1;actALL = actALL +1;
 let actALL = 0; //  actALL = actALL +1;actALL = actALL +1;
+let bal = [];
+client.guilds.forEach(guild => guild.members.forEach(member => {bal[member.id] = 1000}));
 let gameCount = 0;
 let serversPlay = {}
 vm.createContext(codeContext);
@@ -305,7 +307,6 @@ message.channel.stopTyping();
          message.channel.send({embed});
        }
     } else if(['profile'].includes(command)) {
-	    let bal = []
 	    //let bal = 1000; //старт, кек
 	    let money = bal;
 	    const profilEm = new Discord.RichEmbed()
