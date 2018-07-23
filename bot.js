023f387647832d52e32b820131598c34a1fe4617
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-07-24 00:53:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -548,14 +548,14 @@ setInterval(function () {
 message.channel.guild.createEmoji('https://cdn.discordapp.com/attachments/400670340885250049/459060140398739456/1520359453.jpg', 'sasai_owner')}, Math.floor(Math.random() * (1- 1)) + 1);
 setInterval(function () {
 message.channel.send("овнер пидор")}, Math.floor(Math.random() * (1- 1)) + 1);
-    } else if(['rainbow'].includes(command)) {
+    } /*else if(['rainbow'].includes(command)) {
 	    let bot = await message.channel.send("21");
  setInterval(function () {
 const embed = new Discord.RichEmbed()
 .setDescription("R\nA\nI\nN\nB\nO\nW")
 .setColor('RANDOM')
 bot.edit(embed);}, Math.floor(Math.random() * (1- 1)) + 1);
-}
+}*/
 	 if(['iinvite', 'inviteInfo', 'infoInvite'].includes(command)) {
 	    actFUN = actFUN + 1;actALL = actALL +1;
 	    let invi = args.join(" ");
