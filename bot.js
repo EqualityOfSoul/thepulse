69284972794f9b24bffec61bb1d9a72d67e2358f
impulse-x <40152105+impulse-x@users.noreply.github.com>
2018-07-27 11:37:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -456,6 +456,21 @@ voiceChannel.join()
     }
     await reaction.remove(message.author.id);
 		});
+	} else if(['quote'].includes(command)) {
+		message.channel.fetchMessage(args.join(' '))
+    .then(message => {
+    message.channel.send({
+        embed: {
+            title: 'Message quote:',
+            description: `${message.content}`,
+            color: 0x008AF3,
+            footer: {
+                text: `By ${message.author.tag}`,
+                icon_url: message.author.avatarURL
+            }
+        }
+    });
+  }).catch(console.error)
 	} else if (['uptime'].includes(command)) {
 		let up = 'Uptime: ';
   const totalSeconds = process.uptime();
