a91c22d6ca27d10da6a5df6912ef231461ff0b1b
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-07-29 16:38:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2621,6 +2621,35 @@ message.channel.stopTyping()
                 .setColor("#ffd954");
             message.channel.send({embed:embed});
         }
+    } else if(['botinfo'].includes(command)) {
+	    const bot = message.mentions.users.first();
+	    const botid = bot.id;
+	    request('https://discordbots.org/api/bots/' + botid, (e, r, b)=> {
+						let contenu = JSON.parse(b)
+					if(contenu.error === "Not found")  {
+					const embed = new Discord.RichEmbed()
+						embed.setDescription("**Error**\nУказан неправельный бот, либо данного бота нет в discordbotsorg, упомяните нужного вам бота.")
+						embed.setColor("#ffd954")
+						message.channel.send({embed});
+					} else {
+					const embed = new Discord.RichEmbed()
+						embed.setAuthor(contenu.username, "https://cdn.discordapp.com/avatars/"+botid+"/"+contenu.avatar+".png")
+						embed.setThumbnail("https://cdn.discordapp.com/avatars/"+botid+"/"+contenu.avatar+".png")
+						embed.setColor("#ffd954")
+						embed.setTimestamp()
+						embed.addField(contenu.username, contenu.shortdesc, true)
+						embed.addField("Проверенный? <:dblCertified:392249976639455232>", contenu.certifiedBot === true ? "Да <:tickYes:315009125694177281>" : "Нет <:tickNo:315009174163685377>", true)
+						embed.addField("Количество серверов", contenu.server_count, true)
+						embed.addField("Библиотека", contenu.lib, true)
+						embed.addField("Дата добавления", contenu.date, true)
+						embed.addField("Префикс", contenu.prefix, true)
+						embed.addField("Upvotes", contenu.points, true)
+						embed.addField("Создатель(ли)", "<@"+contenu.owners.join(">, <@")+">", true)
+						embed.addField("тег(и)", contenu.tags.length != 0 ? contenu.tags.join(", ") : "Тегов нет", true)
+						embed.addField("Ссылки", "[Invite](" + contenu.invite + "), [DBL.org](https://discordbots.org/bot/" + botid + " ), [Github](" + contenu.github + "), [Website](" + contenu.website + "), [Support Server](https://discordapp.com/invite/" + contenu.support + ")", true)
+message.channel.send({embed});
+    }
+  })
     }
 });
 client.login(process.env.BOT_TOKEN).catch(console.error);
