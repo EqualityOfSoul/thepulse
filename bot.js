365057f0b19ef8a8a1e86f3b788323eee638a688
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-07-30 23:21:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const music = require('discord.js-music-v11');
 const arraySort = require('array-sort');
 const table = require('table');
 const request = require("request");
@@ -44,6 +45,13 @@ let serversPlay = {}
 vm.createContext(codeContext);
 //массив цветов
 const colors = ['ff2828','ff3d28','ff4b28','ff5a28','ff6828','ff7628','ff8c28','ffa128','ffac28','ffb728','ffc228','ffd028','ffd728','ffe228','fff028','fffb28','edff28','deff28','d0ff28','c2ff28','b3ff28','9aff28','8cff28','7dff28','6fff28','5aff28','3dff28','28ff2b','28ff41','28ff56','28ff6c','28ff81','28ff93','28ffa9','28ffba','28ffc9','28ffde','28fff4','28ffff','28f0ff','28deff','28deff','28d3ff','28c5ff','28baff','28b0ff','28a5ff','289eff','2893ff','2885ff','2876ff','2864ff','2856ff','284bff','2841ff','2836ff','2828ff','3228ff','4428ff','5328ff','6828ff','7628ff','7e28ff','8828ff','9328ff','a128ff','b028ff','be28ff','c928ff','d328ff','db28ff','e528ff','f028ff','ff28ff','ff28f7','ff28e5','ff28de','ff28d0','ff28c9','ff28ba','ff28b3','ff28a5','ff289a','ff288c','ff2881','ff287a','ff2873','ff2868','ff2861','ff2856','ff284f','ff2848','ff2844','ff282b'];
+music(client, {
+	prefix: 'x!!',     
+	global: false,     
+	maxQueueSize: 10,  
+	clearInvoker: true, 
+    channel: 'music'   
+});
 /*const Sharder = require('eris-sharder').Master;
 const sharder = new Sharder(process.env.BOT_TOKEN, "/src/main.js", {
 clientOptions: {
