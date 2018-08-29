1a1aafc27353bb9debfa8aacd3cd30e87a97c0a5
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-08-29 18:16:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3747,9 +3747,8 @@ message.channel.stopTyping()
 			message.channel.send(`У вас недостаточно опыта, вам нужно ${NeedXp} но у вас только ${xp}, наберите еще ${NeedXp - xp} и обратитесь еще раз.`)
 		con.query(`UPDATE xp SET lvl = lvl++ WHERE id = '${message.author.id}'`)
 			con.query(`UPDATE xp SET xp = NeedXp - xp WHERE id = '${message.author.id}'`)
-	  });
 	  }
-	}
+	});
 }
 });
 client.login(process.env.BOT_TOKEN).catch(console.error);
