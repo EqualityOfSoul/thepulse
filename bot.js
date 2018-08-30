2a6dabe22f53101c15f8776a82ab5284c20bbccb
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-08-30 21:14:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1542,7 +1542,7 @@ message.channel.send(`Варны для пользователя ${member} на 
 	    if(args[0] === 'set' && args[1] === 'bg') {
 		    let member = message.mentions.members.first();
 		    if(!member) return message.channel.send("Указать забыл");
-		    args.shift(1);
+		    args.shift(2);
 		    let a = args.join(" ");
 		    con.query(`SELECT * FROM xp WHERE id = '${member.user.id}'`, (err, rows) => {
 			    if(!rows) return message.channel.send("У пользователя нет аккаунта.");
