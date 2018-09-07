5694f8a9c98c3a8e516755cb9fb72b38ac6615e2
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-09-07 22:15:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,8 +74,8 @@ con.connect(err => {
 	if (err) throw err;
   console.log("connected")
 })
-const sh = require("weeb.js");
-const weeb = new sh("Wolke "+process.env.WEEB_KEY,"xeval/1.0");
+const sh = require('wolken');
+const weeb = new sh(process.env.WEEB_KEY, "Wolke", "xeval/1.0");
 //пособие о том, как плодить шарды
 /*const Sharder = require('eris-sharder').Master;
 const sharder = new Sharder(process.env.BOT_TOKEN, "/src/main.js", {
