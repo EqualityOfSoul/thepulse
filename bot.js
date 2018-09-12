cd7e364932fbb545011a0e4377c4073045905e8c
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-09-12 12:55:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -468,66 +468,7 @@ function hexToDec(hex) {
     
          message.channel.send({embed});
        }
-    } if(['save'].includes(command)) {
-	    
-	    message.channel.send("**Disclaimer:** ваш ключ сохранен не навсегда, ключ будет удален при перезапуске бота.");
-			if(args.length < 2){
-				message.channel.send(`Сохраните сообщение в ключ \`${prefix}save <key> <message>\``);
-				return;
-			}
-			var key = args[0];
-			var messageToSave = "";
-			for(var i = 0; i < args.length - 2; i++){
-				messageToSave += args[i + 1] + " ";
-			}
-			messageToSave += args[args.length - 1];
-			fs.readFile("save.json", "utf8", function(err, data){
-				if(err) throw err;
-				var save = JSON.parse(data);
-				if(save[message.author.username] === undefined){
-					save[message.author.username] = {};
-				}
-				save[message.author.username][key] = messageToSave;
-				fs.writeFile("save.json", JSON.stringify(save), "utf8", function(err){
-					if(err) throw err;
-					message.channel.send(`Ваше сообщение сохранено под ключем \`${key}\`! :tada:`);
-				});
-			});
-		} else if(['view'].includes(command)) {
-			fs.readFile("save.json", "utf8", function(err, data){
-				if(err) throw err;
-				var save = JSON.parse(data);
-				if(args.length === 0){
-					var messageKeys;
-					var savedMessages = "";
-					try{
-						messageKeys = Object.keys(save[message.author.username]);
-					} catch(e){
-						message.reply("У вас еще нет ключей, но вы можете их создать.");
-						return;
-					}
-					if(messageKeys.length === 0){
-						message.reply("У вас еще нет ключей, но вы можете их создать.");
-						return;
-					}
-					for(var i = 0; i < messageKeys.length - 1; i++){
-						savedMessages += messageKeys[i] + ", ";
-					}
-					savedMessages += messageKeys[messageKeys.length - 1];
-					message.reply("вот ваши ключи: **" + savedMessages + "**")
-				} else{
-					var key = args[0];
-					var recalledMessage;
-					try{
-						recalledMessage = save[message.author.username][key];
-					} catch(e){
-						message.reply(`У вас нет ключей именуемые \`${key}\``)
-						return;
-					}
-					message.channel.send(`${key}: ${recalledMessage}`);
-				}
-			});
-		} 
+    } 
 	if(['osu'].includes(command)) {
 		let mode = args[0];
 		let user = args[1] + ' ' + args[2];
