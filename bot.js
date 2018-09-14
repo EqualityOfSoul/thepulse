f43808abc7ca2063e05c3a75046ea2f2ddb72d37
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-09-14 14:09:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3845,11 +3845,10 @@ message.channel.stopTyping()
 	}
 	if(args[0] ==='create') {
 		con.query(`SELECT * FROM welcome WHERE guild = '${message.guild.id}'`, (err, rows) => {
-			if(rows) return message.channel.send("Кажись у вас уже есть приветсвие");
-
+			if(rows[0]) return message.channel.send("Кажись у вас уже есть приветсвие");
+		});
 		con.query(`INSERT INTO welcome (guild, channel, message, title, color) VALUES ('${message.guild.id}', '${message.channel.id}', 'Текст приветсвия не настроен, пожалуйста найстройте title, color, message', 'Welcome title!', '00ff00')`)
 			message.channel.send("Создано! Теперь настройте welcome `x!welcome argument` \nАргументы: title, message, color, channel");
-		})
 	}
 	if(args[0] ==='test') {
 		let member = message.author;
